6da2005db06eb9f7745202011fd3d2471f046b24
bloodyrainday <bloodyrainday@gmail.com>
2024-06-03 13:22:56 +0300

    second commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -35,8 +35,8 @@ const Card = ({
           className={`${styles.card__icon}`}
           src={
             favoriteItems.some((item) => item.id === id)
-              ? "./images/fav-icon-clicked.svg"
-              : "./images/fav-icon.svg"
+              ? "images/fav-icon-clicked.svg"
+              : "images/fav-icon.svg"
           }
           alt="fav-icon"
           onClick={() => {
@@ -65,8 +65,8 @@ const Card = ({
             className={styles.card__btn}
             src={
               cartItems.some((item) => item.id === id)
-                ? `./images/add-btn-clicked.svg`
-                : `./images/add-btn.svg`
+                ? `images/add-btn-clicked.svg`
+                : `images/add-btn.svg`
             }
             alt="add-btn"
             onClick={() => {
