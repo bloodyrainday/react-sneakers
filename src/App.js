b78751151f8dad6debe83055e92e541bad3527b3
bloodyrainday <bloodyrainday@gmail.com>
2024-06-04 17:07:18 +0300

    22th commit

--- src/App.js
+++ src/App.js
@@ -99,9 +99,9 @@ function App() {
           countTotalPrice={countTotalPrice}
         />
         <Routes>
-          <Route path="react-sneakers" element={<MainContent />}></Route>
-          <Route path="fav" element={<Favorites />}></Route>
-          <Route path="orders" element={<Orders />}></Route>
+          <Route path="./react-sneakers" element={<MainContent />}></Route>
+          <Route path="./fav" element={<Favorites />}></Route>
+          <Route path="./orders" element={<Orders />}></Route>
         </Routes>
       </div>
     </SearchContext.Provider>
