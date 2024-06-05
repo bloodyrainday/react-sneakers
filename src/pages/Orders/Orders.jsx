803ab128e2c49a0f6a500533ee2cc015a03786cd
bloodyrainday <bloodyrainday@gmail.com>
2024-06-05 11:01:55 +0300

    42th commit

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -9,13 +9,24 @@ const Orders = () => {
   const { orderedItems } = React.useContext(SearchContext);
 
   return (
-    <section className={styles.orders}>
+    <section
+      className={styles.orders}
+      style={{ height: `${orderedItems.length !== 0 ? "100%" : "100vh"}` }}
+    >
       <div className="container orders__container">
         <h1 className={styles.orders__title}>your orders</h1>
         <ul className={styles.orders__list}>
-          {orderedItems.map((item, index) => {
-            return <Card {...item} key={`${item}__${index}`} />;
-          })}
+          {orderedItems.length !== 0 ? (
+            orderedItems.map((item, index) => {
+              return <Card {...item} key={`${item}__${index}`} />;
+            })
+          ) : (
+            <h3
+              style={{ width: "100vh", fontSize: "30px", textAlign: "center" }}
+            >
+              currently this page is empty
+            </h3>
+          )}
         </ul>
       </div>
     </section>
